e393711fc6751dc18570fe1adbbd37f89b3f1d3a
bojun <bojun@huskers.unl.edu>
2019-11-06 08:39:32 +0000

    Update Book.java for adding the on hold list

--- src/main/java/edu/unl/cse/csce361/book_management/blackboard/Book.java
+++ src/main/java/edu/unl/cse/csce361/book_management/blackboard/Book.java
@@ -1,7 +1,14 @@
 package edu.unl.cse.csce361.book_management.blackboard;
 
+import java.util.ArrayList;
+
 public interface Book extends Comparable {
     String getCallNumber();
     String getAuthor();
     String getTitle();
+    String getStatus();
+    ArrayList<String> getStringArrayList();
+    void AddpatronOnHoldArrayList(String name);
+    void DeletepatronOnHoldArrayList(String name);
+
 }
