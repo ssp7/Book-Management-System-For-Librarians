651f9d256c1be12db38810385ed12ea4a30192bb
bojun <bojun@huskers.unl.edu>
2019-11-10 20:36:44 +0000

    Add new file

---
+++ src/main/java/edu/unl/cse/csce361/book_management/commands/manipulateHoldList.java
@@ -0,0 +1,18 @@
+package edu.unl.cse.csce361.book_management.commands;
+
+import edu.unl.cse.csce361.book_management.blackboard.BlackboardClass;
+
+public class manipulateHoldList implements Command {
+    BlackboardClass blackboard = new BlackboardClass();
+
+    @Override
+    public void execute() {
+        blackboard.manipulateHoldList();
+
+
+    }
+    @Override
+    public String toString() {
+        return "manipulate Hold List";
+    }
+}
