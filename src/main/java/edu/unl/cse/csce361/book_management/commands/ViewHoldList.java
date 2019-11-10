9ab9bf2ad563a7f6eb2b69957ff8f1a05f857cbf
bojun <bojun@huskers.unl.edu>
2019-11-10 20:37:16 +0000

    Add new file

---
+++ src/main/java/edu/unl/cse/csce361/book_management/commands/ViewHoldList.java
@@ -0,0 +1,19 @@
+package edu.unl.cse.csce361.book_management.commands;
+
+import edu.unl.cse.csce361.book_management.blackboard.BlackboardClass;
+
+public class ViewHoldList implements Command {
+    BlackboardClass blackboard = new BlackboardClass();
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        blackboard.ViewHoldList();
+    }
+
+    @Override
+    public String toString() {
+        return "View Hold List";
+
+    }
+}
