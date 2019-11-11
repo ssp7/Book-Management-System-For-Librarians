e08034e4321c24e45847d8ec3c98326bdab2354e
bojun <bojun@huskers.unl.edu>
2019-11-11 02:48:14 +0000

    Delete library_patron.java

--- src/main/java/edu/unl/cse/csce361/book_management/Observe/library_patron.java
+++
@@ -1,8 +0,0 @@
-package edu.unl.cse.csce361.book_management.Observe;
-
-public class library_patron extends ObservedObject {
-    @Override
-    public void update() {
-        System.out.println(getString1());
-    }
-}
