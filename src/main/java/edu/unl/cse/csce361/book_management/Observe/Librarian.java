bc551bce4b732eeb308ee30b757c2c872771d58f
bojun <bojun@huskers.unl.edu>
2019-11-11 02:47:55 +0000

    Delete Librarian.java

--- src/main/java/edu/unl/cse/csce361/book_management/Observe/Librarian.java
+++
@@ -1,8 +0,0 @@
-package edu.unl.cse.csce361.book_management.Observe;
-
-public class Librarian extends ObservedObject   {
-    @Override
-    public void update() {
-        System.out.println(getString1());
-    }
-}
