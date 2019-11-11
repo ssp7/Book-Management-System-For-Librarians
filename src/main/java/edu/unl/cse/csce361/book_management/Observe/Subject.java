0769f542847d229c41089f4df3ba006f47550523
bojun <bojun@huskers.unl.edu>
2019-11-11 02:48:06 +0000

    Delete Subject.java

--- src/main/java/edu/unl/cse/csce361/book_management/Observe/Subject.java
+++
@@ -1,7 +0,0 @@
-package edu.unl.cse.csce361.book_management.Observe;
-
-public interface Subject {
-    void registerObserver(ObservedObject observer);
-    void removeObserver(ObservedObject observer);
-    void notifyObservers();
-}
