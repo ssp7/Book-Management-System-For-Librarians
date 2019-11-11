9168049decc1e5df5ad7a7f23bb34c2401bf1226
ssp <ssp@huskers.unl.edu>
2019-11-10 18:11:56 -0600

    Missed file

--- src/main/java/edu/unl/cse/csce361/book_management/commands/PrintSpecificPatronInfo.java
+++ src/main/java/edu/unl/cse/csce361/book_management/commands/PrintSpecificPatronInfo.java
@@ -1,11 +1,13 @@
 package edu.unl.cse.csce361.book_management.commands;
 
-public class PrintSpecificPatronInfo implements Command {
+import edu.unl.cse.csce361.book_management.blackboard.BlackboardClass;
 
+public class PrintSpecificPatronInfo implements Command {
+BlackboardClass blackboard = new BlackboardClass();
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		
+		blackboard.PrintSpecificPatronInfo();
 	}
 
 	@Override
